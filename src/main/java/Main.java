01c7047c082f2daed78284695b96bbb02c215528
jamesaud <jamaudre@indiana.edu>
2017-09-04 16:26:15 -0400

    cleaned up function and added toLowerCase for better caching

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,58 +16,58 @@ public class Main {
     final static String errorKeyword = "errors";
 
     static String findPath(String searchTerm){
-            /* Finds a path from a wikipedia article to philosophy.
-             * Stores the path in a DB,  caches response if possible. Try/Catch ensures that the code works even if the DB stops working.
-             * Should return a json formatted String
-             * */
-            String response;
-            String startUrl = "/wiki/" + searchTerm;
+        /* Finds a path from a wikipedia article to philosophy.
+         * Stores the path in a DB,  caches response if possible. Try/Catch ensures that the code works even if the DB stops working.
+         * Should return a json formatted String
+         * */
+        searchTerm = searchTerm.toLowerCase();
+        String response = null;
+        String startUrl = "/wiki/" + searchTerm;
+        HashMap<String, Object> results = new HashMap<>();    // Make a json like object with HashMap for JsonUtil to convert
 
-            // Make a json like object with HashMap for JsonUtil to convert
-            HashMap<String, Object> results = new HashMap<>();
+        String result = null;
+        try {   // Try to get path from DB
+            result = DbUtil.get_from_db(searchTerm);
+        }
+        catch (Exception e){}  // If DB isn't working, ignore
 
-            String result = null;
-
-            // Try to get path from DB
+        if (result != null) { // Return cached path from the DB
             try {
-                result = DbUtil.get_from_db(searchTerm);
+                response = JsonUtil.fromJson(String.format("{%s: %s}", resultKeyword, result)).toString();
+            } catch (JSONException e) {   // Failed to convert to json
+                results.put(errorKeyword, "Internal Server error (" + e.toString() + ")");
+                response = JsonUtil.toJson(results);
             }
-            catch (Exception e){}  // If DB isn't working ignore
+        } else {  // Find the path if not in DB
+            LinkedList<String> path = null;
+            boolean error = false;
+            try {
+                path = new Pathfinder(startUrl).findPath("/wiki/Philosophy"); // null means there's no path
+            } catch (IOException e) {
+                // Trouble connecting to Wikipedia
+                // This can happen if the wikipedia url is malformed
+                results.put(errorKeyword, "Can't find path, error connecting to Wikipedia. Did you provide a proper term?"
+                        + "https://wikipedia.org/wiki/" + searchTerm + " should exist. " + "(" + e.toString() + ")");
+                response = JsonUtil.toJson(results);
+                error = true;
+            }
 
-            if (result != null) { // Sucessfully got the path from the DB
-                try {
-                    response = JsonUtil.fromJson(String.format("{%s: %s}", resultKeyword, result)).toString();
-                } catch (JSONException e) {   // Failed to convert to json
-                    results.put(errorKeyword, "Internal Server error (" + e.toString() + ")");
-                    return JsonUtil.toJson(results);
-                }
-            } else {  // Find the path
-                LinkedList<String> path;
-                try {
-                    path = new Pathfinder(startUrl).findPath("/wiki/Philosophy");
-                } catch (IOException e) {
-                    // Trouble connecting to Wikipedia
-                    results.put(errorKeyword, "Can't find path, error connecting to Wikipedia. Did you provide a proper term?"
-                            + "https://wikipedia.org/wiki/" + searchTerm + " should exist. " + "(" + e.toString() + ")");
-                    return JsonUtil.toJson(results);
-                }
-
+            if (!error) {
                 if (path == null) {
                     results.put(errorKeyword, "No way to reach Philosophy");
                 } else {
-                    Collections.reverse(path);  // Put path into the correct order
+                    Collections.reverse(path);
                     results.put(resultKeyword, path);
-                    // Insert into the DB
                     try {
                         DbUtil.insert_in_db(searchTerm, JsonUtil.toJson(path));
-                    }
-                    catch (Exception e){  } // Trouble connecting to DB, should just return the answer to the user
+                    } catch (Exception e) {} // Trouble connecting to DB, just return the answer to the user
                 }
 
                 response = JsonUtil.toJson(results);
             }
+        }
 
-            return response;
+        return response;
     }
 
     static int getHerokuAssignedPort() {
